d45b7d0c67e7f8a99978d02251b9ef73d8ca8e6a
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-09-04 12:11:50 +0600

    Create Teris in React

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Budget } from './Budget';
-export { default as Remaining } from './Remaining';
-export { default as ExpenseTotal } from './ExpenseTotal';
-export { default as ExpenseList } from './ExpenseList';
-export { default as AddExpenseForm } from './AddExpenseForm';
